4dcfa9ce36a61bd9f3ced60de87f123c7e7a0d72
Vanesflat <ptptcptptc@yandex.ru>
2023-05-10 14:10:13 +0700

    Implement routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,55 @@
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import BookingPage from '../../pages/booking-page/booking-page';
+import ContactsPage from '../../pages/contacts-page/contacts-page';
+import LoginPage from '../../pages/login-page/login-page';
+import MainPage from '../../pages/main-page/main-page';
+import MyQuestsPage from '../../pages/my-quests-page/my-quests-page';
+import QuestPage from '../../pages/quest-page/quest-page';
+import PrivateRoute from '../private-route/private-route';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Main}
+            element={<MainPage />}
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<LoginPage />}
+          />
+          <Route
+            path={AppRoute.Contacts}
+            element={<ContactsPage />}
+          />
+          <Route
+            path={AppRoute.Quest}
+            element={<QuestPage />}
+          />
+          <Route
+            path={AppRoute.MyQuests}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+                <MyQuestsPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Booking}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+                <BookingPage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
 }
 
 export default App;
