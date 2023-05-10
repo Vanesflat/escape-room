8ed03526aa24e9e95c7aacb8588ccd91a4321ef4
Vanesflat <ptptcptptc@yandex.ru>
2023-05-10 14:07:15 +0700

    Add header and footer components

---
+++ project/src/components/header/header.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
+function Header(): JSX.Element {
+  return (
+    <header className="header">
+      <div className="container container--size-l">
+        <span className="logo header__logo">
+          <svg width="134" height="52" aria-hidden="true">
+            <use xlinkHref="#logo"></use>
+          </svg>
+        </span>
+        <nav className="main-nav header__main-nav">
+          <ul className="main-nav__list">
+            <li className="main-nav__item">
+              <Link className="link" to={AppRoute.Main}>Квесты</Link>
+            </li>
+            <li className="main-nav__item">
+              <Link className="link" to={AppRoute.Contacts}>Контакты</Link>
+            </li>
+            <li className="main-nav__item">
+              <Link className="link" to={AppRoute.MyQuests}>Мои бронирования</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="header__side-nav">
+          <Link className="btn btn--accent header__side-item" to={AppRoute.Login}>Выйти</Link>
+          <a className="link header__side-item header__phone-link" href="tel:88003335599">8 (000) 111-11-11</a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
