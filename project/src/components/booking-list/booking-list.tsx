8fa7b690dfb1f93d88749963b00d4efb5aac8dd6
Vanesflat <ptptcptptc@yandex.ru>
2023-05-12 14:13:51 +0700

    Add list empty component and its display logic

--- project/src/components/booking-list/booking-list.tsx
+++ project/src/components/booking-list/booking-list.tsx
@@ -1,11 +1,16 @@
 import { BookingQuest } from '../../types/booking-quest';
 import BookingQuestCard from '../booking-card/booking-card';
+import QuestsEmpty from '../quests-empty/quests-empty';
 
 type BookingListProps = {
   bookingQuests: BookingQuest[];
 }
 
 function BookingList({ bookingQuests }: BookingListProps): JSX.Element {
+  if (!bookingQuests.length) {
+    return <QuestsEmpty />;
+  }
+
   return (
     <div className="cards-grid">
       {bookingQuests.map((bookingQuest) => (
--- project/src/components/quests-list/quests-list.tsx
+++ project/src/components/quests-list/quests-list.tsx
@@ -1,11 +1,16 @@
 import { Quest } from '../../types/quest';
 import QuestCard from '../quest-card/quest-card';
+import QuestsEmpty from '../quests-empty/quests-empty';
 
 type QuestsListProps = {
   quests: Quest[];
 }
 
 function QuestsList({ quests }: QuestsListProps): JSX.Element {
+  if (!quests.length) {
+    return <QuestsEmpty />;
+  }
+
   return (
     <div className="cards-grid">
       {quests.map((quest) => (
